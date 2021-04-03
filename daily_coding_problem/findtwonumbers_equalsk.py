4357d59836704745040fb67653ab8aedb2e0c7d1
Nisrin Dhoondia <38102371+mygoal-javadeveloper@users.noreply.github.com>
2021-04-03 07:44:54 +0530

    Add files via upload

---
+++ daily_coding_problem/findtwonumbers_equalsk.py
@@ -0,0 +1,19 @@
+# Given a list of numbers and a number k, return whether any two numbers from the list add up to k.
+#
+# For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
+
+lst = [10, 15, 3, 7]
+k = 17
+
+def findtwonumbers(lst , k):
+    lst.sort()
+    for x in range(len(lst)-1, 0, -1):
+        if lst[x] == k:
+            return True
+        else:
+            temp = abs(lst[x] - k)
+            if temp in lst:
+                return True
+    else:
+        return False
+print(findtwonumbers(lst , k))
